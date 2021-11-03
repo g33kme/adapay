2c2c1f21341a2d60879601b33d67c21b58c4cc1c
raubtier <raubtier@geekme.de>
2021-11-03 17:03:54 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,209 @@
-# adapay
-ADAPay is a free and open-source Cardano (ADA) payment processor script written in PHP
+# ADAPay
+ADAPay is a free and open-source Cardano (ADA) payment processor script written in PHP. 
+You need to run a [cardano-wallet](https://github.com/input-output-hk/cardano-wallet) to work with ADAPay.  
+
+With ADAPay you can simply create invoices for cardano payments in your webshop as well as verify the incoming payments.
+
+Help us improve this project by staking your ADA to our pool with ticker: **GEEK**  
+[GeekMe Stake Pool](https://adapools.org/pool/c13debc5c24d045cf5e2d69c33ff981602ae55d8bded995a6d930836)
+
+
+## 1. Installation: cardano-wallet
+
+⭕ **Very important!**  
+
+It's very easy to run cardano-wallet on your server, but you need to take care that your server has the minimum 
+hardware requirements to run your cardano-node:
+
+- Min 2 vCPU 
+- Min 8GB of RAM (12GB Recommended)
+- 50 GB of disk space (Ideally SSD)
+- Good internet connection (at least 10Mbps)
+
+If your hardware did not meet the minimum requirements your cardano-node may not start/sync 
+and you can not use the API the cardano-wallet will serve nor can use ADAPay.
+
+### Installing cardano-wallet via docker-compose
+
+So, your server is up and running you can simply download the latest docker-compose.yml and save it to your server. 
+Use [docker-compose](https://docs.docker.com/compose/) to quickly spin up cardano-wallet together with supported block producer.
+Here is exemplary docker-compose.yaml combining the latest versions of **cardano-wallet** and **cardano-node**.
+
+We running on Ubuntu 20.04 LTS
+
+```bash
+# Update operation system
+apt-get update 
+apt-get upgrade
+
+# Install docker and docker-compose
+apt-get install docker
+apt-get install docker-compose
+
+# Download latest docker-compose.yml 
+wget https://raw.githubusercontent.com/input-output-hk/cardano-wallet/master/docker-compose.yml
+
+# Start cardano-wallet in Background
+NETWORK=mainnet docker-compose up -d
+```
+
+When your cardano-wallet is up running `NETWORK=mainnet docker-compose up -d` ou can simply check in your Browser the height of the Cardano Blockchain by requesting the following URL:  
+
+```bash
+http://<IP4-SERVER-ADDRESS>:8090/v2/network/information
+```
+
+It may take some time, maybe 1-2 days tilly our cardano-wallet is synced completely. When finish you get some "ready" status in the JSON response when browsing the above URL.
+
+```json
+{"network_tip":{"time":"2021-11-02T17:01:09Z","epoch_number":300,"absolute_slot_number":44306178,"slot_number":69378},"node_era":"alonzo","node_tip":{"height":{"quantity":6450485,"unit":"block"},"time":"2021-11-02T17:00:44Z","epoch_number":300,"absolute_slot_number":44306153,"slot_number":69353},"sync_progress":{"status":"ready"},"next_epoch":{"epoch_start_time":"2021-11-06T21:44:51Z","epoch_number":301}}
+````
+
+⭕ **Very important!**  
+
+[cardano-wallet](https://github.com/input-output-hk/cardano-wallet) creates a public accessible [API](https://input-output-hk.github.io/cardano-wallet/api/edge/) on port 8090 (default). 
+At easiest and best simple create firewall rules to access the cardano-wallet API only from specific IPs. 
+You can create firewalls, mostly free, on popular hosting providers like Hetzner, Vultr or DigitalOcean. Of course you can setup a custom firewal rule 
+in your operating system as well.
+
+**Wanna run cardano-wallet API on https?** 
+
+Simply setup a nginx proxy pass for the served API of the cardano-wallet
+
+```bash
+apt-get install nginx
+
+# create some ssl certs
+openssl req -x509 -nodes -days 5478 -newkey rsa:2048 -keyout /etc/nginx/cert.key -out /etc/nginx/cert.crt
+
+# switch to nginx dir
+cd /etc/nginx/sites-enabled
+
+# download nginx setup for adapay proxy
+wget https://github.com/g33kme/adapay/main/nginx.adapay
+
+# check nginx config and restart
+service nginx configtest
+systemctl restart nginx
+```
+
+You should now be able to browse your cardano-wallet API via https/ssl. Keep in mind that we don't created valid ssl certificates, so 
+you get an insecure warning in your browser. However, we don't verify this on ADAPay.
+
+```bash
+https://<IP4-SERVER-ADDRESS>/v2/network/information
+```
+
+You may also take care to update your firewall settings and block public traffic for Port 443.
+
+
+## 2. Installation: ADAPay
+
+Alright, your cardano-wallet is up and running. Let's have a look to our ADAPay library to easily create invoices and check payments.
+
+This library is installable via [Composer](https://getcomposer.org/):
+
+```bash
+composer require g33kme/adapay
+```
+
+## Requirements
+
+This library requires PHP 7.1 or later.
+
+## How to use ADApay
+With the ADAPay PHP library it's very easy to work with your cardano-wallet API. Check your wallet, craete invoices, check receiving payments ...
+
+### Create or Restore Cardano Wallet
+
+```php
+/*
+ * First we need to create or restore wallet on our cardano-wallet
+ * Keep in mind, that the cardano-node have to be fully synced so your wallet comes up
+ * 
+ * name              = set any name 
+ * mnemonic_sentence = create new or add your existing mnemonic recovery phrase to restore a wallet, 24 words for shelly wallet
+ * passphrase        = set a password
+ * 
+ * You will get an "id" back, save this unique id to use the wallet for ADAPay
+ */
+$wallet = ADAPAY::restoreWallet(array(
+    'name' => 'adapay',
+    'mnemonic' => 'that are just twenty four words that have not meaning and only for adapay as placeholder so dont try to copy this cheers adapay'
+    'passphrase' => 'myAwesomeAdaPayPassword',
+));
+print_r($wallet)
+
+/*
+ * If you already created a wallet you can simply show all created wallets and grab your walletid
+ */
+$wallets = ADAPAY::wallets(array();
+print_r($wallets)
+```
+
+### Create an invoice
+```php
+/*
+ * How to create an invoice
+ * 
+ * pair     = set a pair like ADAUSD, ADAEUR, ADAGBP
+ * amount   = fiat amount, based on the pair your ada amount for the invoice will be calculated
+ * identify = how to identify the paid invoice
+ * death    = in seconds when the invoice expires
+ * address  = set one of your cardano addresses where you want to receive the payment
+ * walletid = set a walletid from your cardano-wallet you created in the first step
+ */
+$invoice = ADAPAY::invoiceCreate(array(
+   'pair' => 'ADAUSD',
+   'amount' => 10,
+   'identify' => array(
+        'type' => 'amount'
+   ), 
+   'death' => 1800,
+   'address' => '',
+   'walletid' => '' 
+));
+print_r($invoice)
+
+/*
+ * Each created cardano invoice return an unique hash
+ * We recommend to save your invoice in a database
+ */
+
+```
+
+### Verify a payment
+```php
+/*
+ * Check you invoice was paid and you got the payment to your cardano wallet
+ * You can simply pass the return parameters you got from your invoice
+ * 
+ */
+$verify = ADAPAY::verifyPayment(array(
+   'hash' => $invoice['hash'],
+   'walletid' => $invoice['walletid'],
+   'ada' => $invoice['ada'],
+   'created' => $invoice['created'],
+   'death' => $invoice['death']
+));
+print_r($verify)
+
+
+if($verify['waiting']) {
+    //Still waiting for the payment, not received
+    
+} else {
+    
+    /*
+     * Payment received
+     * 
+     * Do your database updates, flag invoice as paid etc ...
+     */
+}
+```
+
+
+## 🙏 Supporters
+
+Stake your ADA to our pool with ticker: GEEK  
+[GeekMe Stake Pool](https://adapools.org/pool/c13debc5c24d045cf5e2d69c33ff981602ae55d8bded995a6d930836)
---
+++ source/adapay.php
@@ -0,0 +1,551 @@
+<?php
+
+namespace g33kme\adapay;
+
+class ADAPAY {
+
+    /**
+     * Let's construct!
+     */
+    public  function __construct() {
+
+    }
+
+    /*
+     * $x = hash, walletid, ada, created, death
+     */
+    public static function verifyPayment($x=false) {
+
+        $walletId = !empty($x['walletid']) ? $x['walleid'] : ADAPAY_WALLETID;
+        $stop[] = empty($walletId) ? 'No valid wallet id' : false;
+
+        $ada =  $x['ada'];
+        $stop[] = (!is_numeric($ada) || $ada <= 0) ? 'No valid ada amount' : false;
+
+        $stop[] = (!is_int($x['death']) || $x['death'] <= 0) ? 'No valid death in seconds' : false;
+        $stop[] = (!is_int($x['created']) || $x['created'] <= 0) ? 'No valid created timestamp' : false;
+
+		//Check invoice death
+		$expiredIn = $x['death'];
+        $elapsedTime = time() - $x['created'];
+        $elapsed = ($elapsedTime > $expiredIn) ? true : false;
+        $stop[] = $elapsed ? '❌ Invoice is death, please simply create a new one' : false;
+
+		//You can add more checks by you own
+
+		$stop = ADAPAY::cleanStop($stop);
+		if(!empty($stop)) {
+		    print_r($stop);
+		} else {
+
+            //Checking latest Transaction with exact amomunt
+            $transactions = ADAPAY::walletTransactions(array(
+                'id' => $walletId,
+
+                //The last hour
+                //'start' => date(DATE_ISO8601, time()-3600)
+
+                //check on deathts
+                'start' => date(DATE_ISO8601, time()-$x['death'])
+            ));
+            //print_r($transactions);
+
+            //You can maybe verify status=in_ledger ... however ^^
+
+            //amount is in cardano ada lovelace (similar to satoshi's), 1 ADA = 1 000 000 Lovelace
+            $adaLovelace = $ada * 1000000;
+            $transactionAdas = array_column($transactions, 'amount');
+            //print_r($transactionAdas);
+
+            foreach($transactionAdas AS $k => $v) {
+                $adas[] = $v['quantity'];
+            }
+
+            if(!in_array($adaLovelace, $adas)) {
+                // echo 'Waiting for payment of '.$ada.' ADA ...';
+                return array(
+                    'hash' => $x['hash'],
+                    'waiting' => true,
+                    'received' => false,
+                    'ada' => $ada,
+                    'adalovelace' => $adaLovelace
+                );
+
+            } else {
+
+                //echo '✅ Payment received';
+
+                //Do your database updates, flag invoice as paid etc ...
+                //Important: you need to flag invoice as paid
+
+                return array(
+                    'hash' => $x['hash'],
+                    'waiting' => false,
+                    'received' => true,
+                    'ada' => $ada,
+                    'adalovelace' => $adaLovelace
+                );
+
+            }
+
+        }
+
+    }
+
+    /*
+     * Create an invoice
+     * $x = pair, amount, identify[array], death, address, walletid
+     */
+    public static function invoiceCreate($x=false) {
+
+        //Check if Adapay server is available
+		$stop[] = !ADAPAY::serverAlive() ? 'Paying with ADA is currently not possible, server is not alive' : false;
+
+		$lastPrice = ADAPAY::lastPrice($x['pair']);
+        $stop[] = (!is_numeric($lastPrice) || $lastPrice <=0) ? 'Can not get last ada price for pair: '.$x['pair'] : false;
+
+        $stop[] = (!is_numeric($x['amount']) || $x['amount'] <=0) ? 'No valid fiat amount to create invoice for pair: '.$x['pair'] : false;
+
+        if(empty($x['identify']) OR $x['identify']['type'] == 'amount') {
+
+            if(empty($x['identify']['start'])) {
+                $x['identify']['start'] = 1000;
+            } else {
+                $stop[] = (!is_int($x['identify']['start']) || $x['identify']['start'] <=0) ? 'Set a valid integer start number to identify your invoice via amount' : false;
+            }
+
+            if(empty($x['identify']['end'])) {
+                $x['identify']['end'] = 1000;
+            } else {
+                $stop[] = (!is_int($x['identify']['end']) || $x['identify']['end'] <=0) ? 'Set a valid integer end number to identify your invoice via amount' : false;
+            }
+
+
+        }
+
+        $stop = ADAPAY::cleanStop($stop);
+		if(!empty($stop)) {
+		    print_r($stop);
+		} else {
+
+            $ada = $x['amount'] / $lastPrice;
+
+            if(empty($x['identify']) OR $x['identify']['type'] == 'amount') {
+
+                //magic, get random float number by simple dividing, nice ^^
+                $randNumber = rand($x['identify']['start'], $x['identify']['end']) / $x['identify']['end'];
+            }
+
+            $ada = $ada + $randNumber;
+            $adaLovelace = $ada * 100000000;
+
+            $hash = ADAPAY::randHash(25);
+
+            return array(
+                'hash' => $hash,
+                'amount' => $x['amount'],
+                'ada' => $ada,
+                'adalovelace' => $adaLovelace,
+                'created' => time(),
+                'death' => $x['death'],
+                'address' => $x['address'],
+                'walletid' => $x['walletid'],
+                'identify' => array(
+                    'type' => $x['identify']['type'],
+                    'start' => $x['identify']['start'],
+                    'end' => $x['identify']['end']
+            );
+
+        }
+
+
+    }
+
+    /*
+     * Get information about a cardano address
+     */
+    public static function addressInspect($x=false) {
+
+        return ADAPAY::query(array(
+            'method' => 'GET',
+            'url' => '/addresses/'.$x['id']
+        ));
+
+    }
+
+    /*
+     * Create a cardano address
+     */
+    public static function addressCreate($x=false) {
+
+        /*
+         * Hello, the short answer is that: you can't.
+         *
+         * The long answer is written with more details in the following Cardano Improvement Proposal https://github.com/cardano-foundation/CIPs/blob/master/CIP-1852/CIP-1852.md.
+         * as this affects the ability to restore the wallet from a single seed mnemonic (payments to or from imported addresses would be missed when restoring).
+         * This is a technical limitation of industry standard BIP-44 on which Cardano CIP-1852 is based
+         */
+
+    }
+
+    /*
+     * Restore or create a Cardano Wallet
+     */
+    public static function restoreWallet($x=false) {
+
+        /*
+         * IMPORTANT DISCLAIMER: Using values other than 20 automatically makes your wallet invalid with regards to BIP-44 address discovery.
+         * It means that you will not be able to fully restore your wallet in a different software which is strictly following BIP-44.
+         * Beside, using large gaps is not recommended as it may induce important performance degradations. Use at your own risks.
+         */
+        $x['gap'] = !empty($x['gap']) ? $x['gap'] : 20;
+
+        return ADAPAY::query(array(
+            'method' => 'POST',
+            'url' => '/wallets',
+            'data' => array(
+                'name' => $x['name'],
+                'mnemonic_sentence' => $x['mnemonic'],
+                'passphrase' => $x['passphrase'],
+                'address_pool_gap' => $x['gap']
+            )
+        ));
+
+    }
+
+    /*
+     * List transactions from a wallet
+     */
+    public static function walletTransactions($x=false) {
+
+        $data['start'] = $x['start'];
+        $data['end'] = $x['end'];
+        $data['order'] = $x['order'];
+        $data['minWithdrawal'] = $x['minwithdrawal'];
+
+        return ADAPAY::query(array(
+            'method' => 'GET',
+            'url' => '/wallets/'.$x['id'].'/transactions',
+            'data' => $data
+        ));
+
+    }
+
+    /*
+     * List addresses from a cardano wallet
+     */
+    public static function walletAdresses($x=false) {
+
+        return ADAPAY::query(array(
+            'method' => 'GET',
+            'url' => '/wallets/'.$x['id'].'/addresses'
+        ));
+
+    }
+
+    /*
+     * Get information about a cardano wallet
+     */
+    public static function wallet($x=false) {
+
+        return ADAPAY::query(array(
+            'method' => 'GET',
+            'url' => '/wallet/'.$x['id']
+        ));
+
+    }
+
+    /*
+     * List all available cardano wallets
+     */
+    public static function wallets() {
+
+        return ADAPAY::query(array(
+            'method' => 'GET',
+            'url' => '/wallets'
+        ));
+
+    }
+
+    /*
+     * Network status of cardano-node and cardano-wallet
+     */
+    public static function networkStatus() {
+
+        return ADAPAY::query(array(
+            'method' => 'GET',
+            'url' => '/network/information'
+        ));
+
+    }
+
+    /*
+     * Check if cardano-wallet api is alive
+     */
+    public static function serverAlive() {
+        $data = ADAPAY::networkStatus();
+        return !empty($data) ? true : false;
+    }
+
+    /*
+     * Get last ADA (Cardano) price by a pair
+     * like: ADAEUR, ADAUSD, ADAGBP ...
+     */
+    public static function lastPrice($x=false) {
+        $pairRate = ADAPAY::rates(array('pair' => $x['pair']));
+        return $pairRate['lastPrice'];
+    }
+
+    /*
+     * Get last ADA (Cardano) price in EUR
+     */
+    public static function lastPriceEuro() {
+        $pairRate = ADAPAY::rates(array('pair' => 'ADAEUR'));
+        return $pairRate['lastPrice'];
+    }
+
+    /*
+     * Get last ADA (Cardano) price in USD
+     */
+    public static function lastPriceDollar() {
+        $pairRate = ADAPAY::rates(array('pair' => 'ADAUSD'));
+        return $pairRate['lastPrice'];
+    }
+
+    /*
+     * Get last ADA (Cardano) price in GBP
+     */
+    public static function lastPricePound() {
+        $pairRate = ADAPAY::rates(array('pair' => 'ADAGBP'));
+        return $pairRate['lastPrice'];
+    }
+
+    /*
+     * Get rates e.g for ADAEUR via binance
+     */
+    public static function rates($x=false) {
+
+        $response = ADAPAY::curl(array(
+            'method' => 'GET',
+            'url' => 'https://api.binance.com/api/v1/ticker/24hr'
+        ));
+
+        $rates = json_decode($response, 1);
+
+        if(!empty($x['pair'])) {
+            $k = array_search($x['pair'], array_column($rates , 'symbol'));
+            return $rates[$k];
+        } else {
+            return $rates;
+        }
+
+
+    }
+
+    /*
+     */
+    public static function query($x=false) {
+
+        //print_r($x);
+
+        $x['method'] = !empty($x['method']) ? $x['method'] : 'GET';
+        $x['url'] = ADAPAY_API.$x['url'];
+
+        $response = ADAPAY::curl(array(
+
+            'printcurl' => false,
+            'debug' => false,
+            'info' => false,
+
+            'method' => $x['method'],
+            'url' => $x['url'],
+            'data' => $x['data'],
+
+            'jsonencode' => true,
+
+            'httpheader' => array(
+                'Accept: application/json',
+                'Content-type: application/json'
+            ),
+        ));
+
+        //echo 'Real Response: '.$response;
+        return json_decode($response, 1);
+
+
+    }
+    
+    /**
+	 * cURL Request
+     */
+	public static function curl($x=false) {
+
+        if($x['printcurl']) {
+            print_r($x);
+        }
+
+		$curl = curl_init();
+
+		if($x['debug']) {
+		     $fp = fopen('./curl.log', 'w');
+             curl_setopt($curl, CURLOPT_VERBOSE, 1);
+             curl_setopt($curl, CURLOPT_STDERR, $fp);
+		}
+
+
+		//Async
+		if($x['async']) {
+
+			curl_setopt($curl, CURLOPT_FRESH_CONNECT, true);
+
+			//Do not set TIMEOUT_MS to short, script should be of course executed ;-)
+			curl_setopt($curl, CURLOPT_NOSIGNAL, 1);
+			curl_setopt($curl, CURLOPT_TIMEOUT_MS, 337); //l33t ^^
+		}
+
+
+		//Maybe create some file for sending
+		//Attention! here you set the param key to "file" for sending to a remote API, you may need another key
+		if(!empty($x['file'])) {
+			if(is_file($x['file'])) {
+				if(is_array($x['file'])) {
+					//TODO
+
+				} else {
+					//Create curl file and add to data
+					//echo 'File: '.$x['file'];
+
+					$x['filemime'] = !empty($x['filemime']) ? $x['filemime'] : 'application/octet-stream';
+					$x['filename'] = !empty($x['filename']) ? $x['filename'] : basename($x['file']);
+
+					$cfile = curl_file_create($x['file'], $x['filemime'], $x['filename']);
+
+					//maybeyou have to change the name instead of "file" ...
+					$x['data']['file'] = $cfile;
+				}
+			}
+		}
+
+		//To set any key for file
+		//Here you can set any key for file
+		if(!empty($x['createfile'])) {
+			if(is_file($x['createfile']['value'])) {
+
+                $cfile = curl_file_create($x['createfile']['value'], $x['createfile']['mimetype']);
+                $x['data'][$x['createfile']['key']] = $cfile;
+            }
+		}
+
+
+		switch($x['method']) {
+
+			case "POST":
+
+				curl_setopt($curl, CURLOPT_POST, true);
+
+				if(!empty($x['data'])) {
+
+					//JSON Encode for REST API
+					if($x['jsonencode']) {
+
+                        $dataJsonEncoded = json_encode($x['data']);
+
+						curl_setopt($curl, CURLOPT_POSTFIELDS, $dataJsonEncoded);
+
+						if($x['printcurl']) {
+                            echo $dataJsonEncoded;
+                        }
+
+
+					} else {
+						curl_setopt($curl, CURLOPT_POSTFIELDS, $x['data']);
+
+						if($x['printcurl']) {
+                            print_r($x['data']);
+                        }
+
+					}
+				}
+			break;
+
+			case "PUT":
+				curl_setopt($curl, CURLOPT_PUT, 1);
+			break;
+
+			case "DELETE":
+				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+			break;
+
+			default:
+				if(!empty($x['data'])) {
+					$x['url'] = sprintf("%s?%s", $x['url'], http_build_query($x['data']));
+				}
+			break;
+		}
+
+		// Optional Authentication:
+		if(!empty($x['httpauthuser'])) {
+			curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($curl, CURLOPT_USERPWD, $x['httpauthuser'].':'.$x['httpauthpass']);
+		}
+
+		if(!empty($x['httpheader'])) {
+			curl_setopt($curl, CURLOPT_HTTPHEADER, $x['httpheader']);
+		}
+
+		curl_setopt($curl, CURLOPT_URL, $x['url']);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+		$response = curl_exec($curl);
+		$errno    = curl_errno($curl);
+		$errmsg   = curl_error($curl);
+
+		if($x['info']) {
+		    $info = curl_getinfo($curl);
+            print_r($info);
+		}
+
+		curl_close($curl);
+
+		// ensure the request succeeded
+		if ($errno != 0) {
+			//Do not die on error!
+			//die('Error: '.$errno.$errmsg);
+			//echo 'curl error: '.$errno.', '.$errmsg;
+		}
+
+		//echo 'RealResponse: '.$response;
+		return $response;
+
+	}
+
+	/*
+	 */
+	public static function cleanStop($stop) {
+		return array_unique(array_values(array_filter($stop)));
+	}
+
+    public static function randHash($lng, $numericOnly=false) {
+
+        mt_srand(crc32(microtime()));
+
+        if($numericOnly) {
+            $b = "1234567890";
+        } else {
+            $b = "abcdefghijklmnpqrstuvwxyz1234567890";
+        }
+
+       $str_lng = strlen($b)-1;
+       $rand= "";
+
+       for($i=0;$i<$lng;$i++)
+          $rand.= $b{mt_rand(0, $str_lng)};
+
+       return $rand;
+    }
+
+
+}
+?>
